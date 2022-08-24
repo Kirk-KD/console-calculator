e7f635fa4a8cf2d61574a3e98096261d0c0c0395
Kirk-KD <kirk.ding123@gmail.com>
2022-08-24 08:48:23 -0400

    Small rounding fix

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,5 @@
 def int_or_float(s):
-    return int(float(s)) if float(s) % 1 == 0 else float(s)
+    return int(float(s)) if float(s) % 1 == 0 else round(float(s), 9)
 
 
 def span(style: str, s):
